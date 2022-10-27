81084997cab2fced95e862123db1c4c808a859d7
Aleksandr Aleksandrov <aleksandrov.aleksander.a@gmail.com>
2022-10-27 12:17:41 +0300

    Add player movement and collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
   private BoxCollider2D boxCollider;
-    // Start is called before the first frame update
-    void Start()
+  private Vector3 moveDelta;
+  private RaycastHit2D hit;
+
+  void Start()
+  {
+    boxCollider = GetComponent<BoxCollider2D>();
+  }
+
+  private void FixedUpdate()
+  {
+    float x = Input.GetAxisRaw("Horizontal");
+    float y = Input.GetAxisRaw("Vertical");
+    moveDelta = new Vector3(x, y, 0f);
+    if (moveDelta.x > 0f)
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+      transform.localScale = Vector3.one;
+    }
+    else if (moveDelta.x < 0f)
+    {
+      transform.localScale = new Vector3(-1f, 1f, 1f);
+    }
+    hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0f, new Vector2(0f, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+    if (hit.collider == null)
+    {
+      transform.Translate(0f, moveDelta.y * Time.deltaTime, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0f, new Vector2(moveDelta.x, 0f), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+    if (hit.collider == null)
     {
-        
+      transform.Translate(moveDelta.x * Time.deltaTime, 0f, 0f);
     }
+
+  }
 }
